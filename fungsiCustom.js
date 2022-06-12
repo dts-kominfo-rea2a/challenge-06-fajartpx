8c37668551a4f7e0c62cee3a89256e797f8e2aeb
fajartpx <fajartpx@gmail.com>
2022-06-12 21:06:25 +0700

    Pembuatan Fungsi bacaData

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,9 +1,9 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
-let file1 = "./data1.json";
-let file2 = "./data2.json";
-let file3 = "./data3.json";
+let file1 = './data1.json';
+let file2 = './data2.json';
+let file3 = './data3.json';
 
 // ! JANGAN DIMODIFIKASI
 let modifyFile1 = (val) => {
@@ -18,7 +18,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  arrHasil = [];
+  fs.readFile(file1, { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+
+    const jsonData1 = JSON.parse(data).message.split(' ');
+  
+    arrHasil.push(jsonData1[1]);
+
+    //call dataJson 2
+    fs.readFile(file2, { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        return fnCallback(err, null);
+      }
+
+      const jsonData2 = JSON.parse(data);
+      arrHasil.push(jsonData2[0].message.split('halo ').join(' '));
+
+      //call dataJson 3
+      fs.readFile(file3, { encoding: "utf-8" }, (err, data) => {
+        if (err) {
+          return fnCallback(err, null);
+        }
+        const jsonData3 = JSON.parse(data);
+        arrHasil.push(jsonData3[0]['data'].message.split('halo ').join(' '));
+        fnCallback(null, arrHasil);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
